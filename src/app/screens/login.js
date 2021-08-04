b3c93bcf9e4d1e71feca3ed63682df29710bfc74
treyshedrick <treyshedrick@gmail.com>
2021-08-03 20:05:12 -0500

    feat: navigate home after log in

--- src/app/screens/login.js
+++ src/app/screens/login.js
@@ -1,25 +1,31 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View} from 'react-native';
 import {Layout, Input, Text, Button} from '@ui-kitten/components';
 import * as UA from '../../auth/userLogin';
 import styles from './styles/form';
+import {TouchableWithoutFeedback, Keyboard} from 'react-native';
 
 const Login = ({navigation}) => {
-  const [usernameValue, setUsernameValue] = React.useState('');
-  const [passwordValue, setPasswordValue] = React.useState('');
+  const [usernameValue, setUsernameValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     UA.currentAuthenticatedUser()
       .then(response => {
-        console.log(response.username);
-        if (response.username.length > 1) {
+        if (response?.username.length > 1) {
+          setIsLoggedIn(true);
           navigateHome();
         }
       })
       .catch(err => {
         console.log(err);
+      })
+      .finally(() => {
+        setIsLoaded(true);
       });
-  });
+  }, []);
 
   const navigateSignUp = () => {
     navigation.navigate('Signup');
@@ -32,7 +38,7 @@ const Login = ({navigation}) => {
   const login = (username, password) => {
     UA.signIn(username, password)
       .then(response => {
-        if (response.username.length > 1) {
+        if (response?.username.length > 1) {
           console.log('This should go Home');
           navigateHome();
         }
@@ -44,33 +50,41 @@ const Login = ({navigation}) => {
   };
 
   return (
-    <Layout style={styles.layout}>
-      <Text category="h1" style={styles.center}>
-        NintenLife
-      </Text>
-      <Input
-        style={styles.input}
-        value={usernameValue}
-        onChangeText={nextValue => setUsernameValue(nextValue)}
-        label={evaProps => <Text {...evaProps}>Username</Text>}
-      />
-      <Input
-        style={styles.input}
-        value={passwordValue}
-        onChangeText={nextValue => setPasswordValue(nextValue)}
-        label={evaProps => <Text {...evaProps}>Password</Text>}
-      />
-      <View style={styles.btnRow}>
-        <Button
-          style={styles.btn}
-          onPress={() => login(usernameValue, passwordValue)}>
-          Login
-        </Button>
-        <Button style={styles.btn} onPress={navigateSignUp}>
-          Sign Up
-        </Button>
-      </View>
-    </Layout>
+    <>
+      {isLoaded === true && isLoggedIn === false ? (
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+          <Layout style={styles.layout}>
+            <Text category="h1" style={styles.center}>
+              NintenLife
+            </Text>
+            <Input
+              style={styles.input}
+              value={usernameValue}
+              onChangeText={nextValue => setUsernameValue(nextValue)}
+              label={evaProps => <Text {...evaProps}>Username</Text>}
+            />
+            <Input
+              style={styles.input}
+              value={passwordValue}
+              onChangeText={nextValue => setPasswordValue(nextValue)}
+              label={evaProps => <Text {...evaProps}>Password</Text>}
+            />
+            <View style={styles.btnRow}>
+              <Button
+                style={styles.btn}
+                onPress={() => login(usernameValue, passwordValue)}>
+                Login
+              </Button>
+              <Button style={styles.btn} onPress={navigateSignUp}>
+                Sign Up
+              </Button>
+            </View>
+          </Layout>
+        </TouchableWithoutFeedback>
+      ) : (
+        <Layout style={styles.layout} />
+      )}
+    </>
   );
 };
 
