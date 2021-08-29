1f940433062d22432499083e438f63f10dfb0515
treyshedrick <treyshedrick@gmail.com>
2021-08-28 20:10:09 -0500

    feat: create and ensure posts scroll view

--- src/app/components/screens/Posts.js
+++ src/app/components/screens/Posts.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import {
-  Divider,
-  Layout,
-  Text,
-  TopNavigation,
-  Button,
-} from '@ui-kitten/components';
+import {Divider, Layout, TopNavigation, Button} from '@ui-kitten/components';
 import {StyleSheet, ScrollView} from 'react-native';
 import {EditIcon} from '../shared/icons';
 import pageStyles from './styles/page';
+import PostCard from '../shared/PostCard';
 
 export const PostsScreen = ({navigation}) => {
   const styles = StyleSheet.create({
@@ -16,9 +11,14 @@ export const PostsScreen = ({navigation}) => {
       flex: 1,
       alignItems: 'center',
     },
+    scrollContainer: {
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      margin: 10,
+    },
     button: {
-      bottom: 30,
-      right: 30,
+      bottom: 25,
+      right: 25,
       borderRadius: 50,
       paddingTop: 22,
       paddingBottom: 22,
@@ -35,8 +35,14 @@ export const PostsScreen = ({navigation}) => {
       <TopNavigation title="Posts" alignment="center" />
       <Divider />
       <Layout style={styles.contentContainer}>
-        <ScrollView contentContainerStyle={styles.contentContainer}>
-          <Text category="h4">Posts</Text>
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+          <PostCard
+            post={{
+              title: 'Zelda: Breath of the Wild Review',
+              summary:
+                'This is another test to see if it goes all the way to the width asldfh asd fowier als dfalsdjf asld fa;ls dfa sdf asd fa sdf asd faw',
+            }}
+          />
         </ScrollView>
         <Button
           style={styles.button}
