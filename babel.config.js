803900ad45c76ef9891ca0d7b66b9b72b1540f59
treyshedrick <treyshedrick@gmail.com>
2021-09-12 11:25:48 -0500

    feat: install module resolver and babel config

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [['module:react-native-dotenv']],
+  plugins: [
+    ['module:react-native-dotenv'],
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '~app': './src/app',
+          '~screens': './src/app/components/screens',
+          '~shared': './src/app/components/shared',
+          '~helpers': './src/app/helpers',
+          '~redux': './src/app/redux',
+        },
+      },
+    ],
+  ],
 };
